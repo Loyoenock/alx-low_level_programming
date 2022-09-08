7cd99beb12e03384a7e27a28d92f49282f92848a
root <Loyoenock>
2022-09-08 13:24:19 -0700

    pprintf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,28 +1,14 @@
 include <stdio.h>
 
  /**
- * main - prints string in put
+ * main - printf function
  *
- *Using main function 
- *Program prints "Programming is like building a multilingual Puzzle"
- *Return: 0
+ * Description:  'prints with proper grammar outputs a piece of art'
+ *
+ * Return (0)
  */
- init main(void)
- {
-	 printf("with proper grammar, but the outcome is a piece of art,\n);
-	 return (0);
-
- }
-#include <stdio.h>
-/**
-* main - prints put function string
-*
-*Uses the main function
-*this program prints "programming is like building a multilingual puzzle"
-*Returns - 0
-*/
 int main(void)
 {
-	printf("with propergrammar, but the outcome is a piece of art,\n);
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
