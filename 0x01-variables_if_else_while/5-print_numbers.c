375ffcd2285616a9b300b43e90fd7704dfa64dbc
root <Loyoenock>
2022-09-09 16:33:35 -0700

    intergers 1 t0 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers from 0 to 10
+ *
+ * Description: A function that prints intergers from 0 to 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+	printf("%n", n);
+	}
+	printf("\n");
+	return (0);
+}
