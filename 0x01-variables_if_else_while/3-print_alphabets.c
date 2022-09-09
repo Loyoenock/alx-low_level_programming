39f4880abf85025636749d873064217b1571f4e9
root <Loyoenock>
2022-09-09 15:36:22 -0700

    lower & upper alphs - correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char al;
 
-	for (al = 'a' ; al <=z ; al++)
+	for (al = 'a' ; al <= 'z' ; al++)
 	{
 		putchar(al);
 	}
-	for (al = 'A' ; al <= Z ; all++)
+	for (al = 'A' ; al <= 'Z' ; al++)
 	{
 		putchar(al);
 	}
