7eb76c844384eecf6ba8e3a1bd6ddc485c91813a
root <Loyoenock>
2022-09-09 16:03:15 -0700

    lower alps apart from e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program prints all alphs apart from e and q
+ *
+ * Description: Using putchar function to print all alphs except e and q
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+	{
+		if (al != 'e' && al I= 'q')
+		{
+			putcher(al);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
