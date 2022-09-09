2f347d8b7e9602366ae0e8b021ce18411096be04
root <Loyoenock>
2022-09-09 15:29:49 -0700

    lower & upper alps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+/**
+ * main - this program prints both lower and upper alph letter and begins a line *
+ * Descrition: This is a putchar function the prints alphas.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a' ; al <=z ; al++)
+	{
+		putchar(al);
+	}
+	for (al = 'A' ; al <= Z ; all++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
