1190a3f38fefab82237d31ed4c382efe98473163
root <Loyoenock>
2022-09-09 16:12:34 -0700

    trailing white space fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,12 +4,11 @@
  * main - program prints all alphs apart from e and q
  *
  * Description: Using putchar function to print all alphs except e and q
- * 
+ *
  * Return: 0
  */
 int main(void)
-{
-	
+{	
 	char al;
 
 	for (al = 'a' ; al <= 'z' ; al++)
