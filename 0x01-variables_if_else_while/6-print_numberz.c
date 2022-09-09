4011d36a5f12789c7c99a226e1e807bef353872e
root <Loyoenock>
2022-09-09 16:58:07 -0700

    count 10 to 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Description : Using a for function to print digits 10 to 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar('\n');
+		return (0)
+	}
+}
