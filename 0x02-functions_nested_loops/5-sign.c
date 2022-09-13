cdd4dd65647027e8981a06b2989fde1e3573e2f7
root <Loyoenock>
2022-09-13 13:42:39 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 int print_sign(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -17,10 +17,9 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar('0');
 		return (0);
 	}
-
 }
