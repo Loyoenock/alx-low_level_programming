96b59d657f910f28bb24e5ae2f9f06c5a8ad7428
root <Loyoenock>
2022-09-13 12:00:16 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * function that prints alphabeth 10x
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	char a, i;
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n')
+	}
+}
