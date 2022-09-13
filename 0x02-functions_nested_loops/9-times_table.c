4e01a7eb7ffbb2280a7d634b1203719176a2ba1c
root <Loyoenock>
2022-09-13 15:28:32 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void times_table - Multples of 9 0 inclusive
+ * times_table - Multples of 9 0 inclusive
  */
 void times_table(void)
 {
@@ -14,7 +14,7 @@ void times_table(void)
 		{
 		_putchar(',');
 		_putchar(' ');
-		
+	
 		p = n * m;
 
 		if (p <= 9)
