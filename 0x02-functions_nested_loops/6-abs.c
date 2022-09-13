79dc793ca6240b53f01156f6f81af067af92c15e
root <Loyoenock>
2022-09-13 14:07:51 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computing the absolute value of integer
+ * @c: int for function agrgument
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c ==0)
+	{
+		return(c);
+	}
+	else 
+		return (c * -1);
+}
