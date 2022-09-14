7d2b0d90a7a464df4bdb3121be6bcb03f6a703b2
root <Loyoenock>
2022-09-14 12:11:14 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * *main - prints count of multiple
+ * Description: a function the computes and print multiples of 3 & 5 > 1024.
+ * Return: return 0
+ */
+{
+	int n, sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
