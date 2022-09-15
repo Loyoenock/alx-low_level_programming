3e804287fe7a5db2a0983b5439f2325bdaa8af8c
root <Loyoenock>
2022-09-15 11:22:12 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - printing a triangel
+ * @size: triangel pramater
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int t1, t2;
+
+	if (size > 0)
+	{
+		for (t1 = 1; t1 <= size; t1++)
+		{
+			for ((t2 = size - t1); t2 > 0; t2--)
+			{
+				putchar(' ');
+			}
+			for (t2 = 0; inc2 < t1; t2++)
+			{
+				putchar('#');
+			}
+			if (t1 == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
