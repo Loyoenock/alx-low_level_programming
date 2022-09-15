a24fc14881559fa45a888de529a4a5e383b61ab0
root <Loyoenock>
2022-09-15 10:31:11 -0700

    Printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - drawing squares
+ * @size: parm
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int s1, s2;
+
+	if (size > 0)
+	{
+		for (s1 = 0; s1 < size; s1++)
+		{
+			for (s2 = 0; s2 < (size - 1); s2++)
+			{
+				putchar('#');
+			}
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
