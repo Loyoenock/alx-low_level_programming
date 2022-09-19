347310002980acdd9c15504ffbfeeb6e97a4b090
root <Loyoenock>
2022-09-19 11:55:06 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,12 @@ void puts_half(char *str)
 {
 	int a, b, c;
 
-	a = len(str);
+	a = strlen(str);
 	if (a % 2 == 1)
 		b = a / 2 + 1;
 	else
 		b = a / 2;
 	for (c = b; c < a; c++)
-		_putchar(str[c]);
+		putchar(str[c]);
 	_putchar('\n');
 }
