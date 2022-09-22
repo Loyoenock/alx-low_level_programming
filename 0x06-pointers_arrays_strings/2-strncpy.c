75cd959590c41b135dc1d970515746724d43cb48
root <Loyoenock>
2022-09-22 12:50:05 -0700

    main.h edited

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+
 /**
  *_strncpy - copies up to n characters from the string pointed
  *
@@ -12,11 +13,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
+	
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
+
+
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sdio.h>
 /**
  *void prototypes()
  *int prototypes()
