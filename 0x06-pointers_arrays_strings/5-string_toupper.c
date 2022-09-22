a4fff32f1ef9e8c1c911eec9a0ec9612d06b3049
root <Loyoenock>
2022-09-22 14:43:47 -0700

    files added

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  */
 char *string_toupper(char *)
 {
-	int s = 0;
+	int i = 0;
 
-	while (str[s] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[s] >= 'a' && str[s] <= 'z')
-			str[s] -= 32;
-		s++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
