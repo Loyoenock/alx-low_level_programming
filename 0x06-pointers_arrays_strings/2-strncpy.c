ccfc56f65b8c80029f5d7de8780bc6b14b111818
root <Loyoenock>
2022-09-22 12:58:20 -0700

    copies string 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  *_strncpy - copies up to n characters from the string pointed
@@ -13,7 +12,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
