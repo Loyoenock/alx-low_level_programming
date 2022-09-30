e0ac625da4dc8a175201ac37fa01fddd45989b6b
root <Loyoenock>
2022-09-30 06:04:32 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: counts multiples in argv
+ * @argv: stores multiples from argc
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+if (argc == 3)
+{
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+	return (0);
+}
+	printf("Error\n");
+	return (0);
+}
