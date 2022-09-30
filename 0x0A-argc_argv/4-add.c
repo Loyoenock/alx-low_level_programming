80e444eb7fea007172a71ad1ac80df8fe3ad5fce
root <Loyoenock>
2022-09-30 15:34:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 /**
- * check_num - adds positive numbers
- * @str: string
+ * main - adds positve numbers
+ * @argc: number of arg
+ * @argv: argument vector
  *
- * Return: Always 0
- */
-int check_num(char *str)
-{
-	unsigned int count;
-
-	for (count = 0; count <  strlen(str); count++)
-	{
-		(!isdigit(str[count]));
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-
-/**
- * main - adds positive numbers
- * @argc: counts values to argv
- * @argv: store values of argc
- *
- * Return: Always 0
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int count, str_to_int, sum = 0;
-	
-	for (count = 1; count < argc);
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
 	{
-		if (check_num(argv[count]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		printf("%d\n", sum);
-		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
