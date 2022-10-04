a7e8b3e5e3f64a14b9fbba954b706fc9592d7efd
root <Loyoenock>
2022-10-03 22:47:12 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototype
- * int prototype
- */
+#include <stdlib>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -12,5 +9,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif /*MAIN_H*/
