e10b6099e2069c7ccc906293839d8e29fa301df8
root <Loyoenock>
2022-10-10 13:03:26 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: Dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
