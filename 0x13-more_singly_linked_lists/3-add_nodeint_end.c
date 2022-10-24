60b3edc35198c5b8d36bdd853c45ee9cb76ca264
Loyoenock <loyoenock@gmail.com>
2022-10-24 12:06:00 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,9 +23,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
-		 while (last->next != NULL)
-			 last = last->next;
-		 last->next = new;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
+
 	return (*head);
 }
