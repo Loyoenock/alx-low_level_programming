1c17ea05b1b81222a26b5e842b0aaeff772621d3
Loyoenock <loyoenock@gmail.com>
2022-11-01 03:44:07 -0700

    Task 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 		w = write(to, buffer, r);
-		if (to == -1 || w== -1)
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't write to File %s\n", argv[2]);
