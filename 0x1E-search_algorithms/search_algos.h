1e68d15570143cdc276b51de1bc4a6403e83b162
Loyoenock <loyoenock@gmail.com>
2023-07-12 05:33:00 -0700

    Project header file added

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,47 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Singly linked list.
+ *
+ * @n: int
+ * @index: index of the node in the list.
+ * @next: Noode pointer.
+ */
+typedef struct listint_s
+{
+                int n;
+                size_t index;
+                struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane.
+ *
+ * @n: Int
+ * @index: Index of the node in the list
+ * @next: Next node pointer
+ * @express: Pointer to next node the express lane
+ */
+typedef struct skiplist_s
+{
+                                int n;
+                                size_t index;
+                                struct skiplist_s *next;
+                                struct skiplist_s *express;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif /* SEARCH_ALGOS_H */
