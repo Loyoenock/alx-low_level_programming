feb6f1ab62bfb298b06e2aea26e3c787f8c5cf88
Loyoenock <loyoenock@gmail.com>
2023-07-12 06:15:12 -0700

    Trailing white space and space before the tabs error fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,29 +1,29 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * linear_search - for a value in a sorted array of integers              
- * @array:  Pointer to the first search element.
- * @size: Number of elements in array
- * @value: value to search for
+ * linear_search - Searches for a value in an array of integers using
+ *		linear search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
  *
- * Return: -1, If the value is not present or the array is NULL
- *         else, first index where the value is located.
+ * Return: Index of the value if found, -1 if not found or array is NULL
  *
  * Description: Prints a value each time it's compared in the array.
  */
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i;
+	size_t i;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        for (i = 0; i < size; i++)
-        {
-                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        return (i);
-        }
-
-        return (-1);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
